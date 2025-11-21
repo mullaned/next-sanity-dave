06ad940a34b2169e5b043fef2e5aa29394a4ab37
David Mullaney <david.mullaney@deptagency.com>
2025-11-21 15:46:04 +0000

    fix: update formatting

--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -2,7 +2,6 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 export default async function Header() {
- 
   return (
     <header className="fixed z-50 h-24 inset-0 bg-white/80 flex items-center backdrop-blur-lg">
       <div className="container py-6 px-2 sm:px-6">
